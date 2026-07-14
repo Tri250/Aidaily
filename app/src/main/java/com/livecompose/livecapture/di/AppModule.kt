8c549e3c62a5deccab31c44ee60c668ad8c7abf3
Tri250 <Tri250@users.noreply.github.com>
2026-07-14 13:34:05 +0000

    chore: 清空项目仓库所有文件，恢复空白状态

--- app/src/main/java/com/livecompose/livecapture/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package com.livecompose.livecapture.di
-
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule
--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file
-plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
-}
